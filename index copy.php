c005426bc41f2b7120abc4bf6067323e9cd3caa5
miakojason <miakojason@gmail.com>
2023-12-15 17:18:03 +0800

    修正commit尚未完成的時候，不小心把index 62行<!右邊上面>不小心刪掉div class=di di ad導致下面bottom錯亂

--- index copy.php
+++ index copy.php
@@ -1,4 +1,4 @@
-<?php include_once "./api/db.php";?>
+<?php include_once './api/db.php';?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -12,17 +12,16 @@
 <body>
 <div id="cover" style="display:none; ">
 	<div id="coverr">
-    	<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl('#cover')">X</a>
+    	<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl(&#39;#cover&#39;)">X</a>
         <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
     </div>
 </div>
-<!-- <iframe style="display:none;" name="back" id="back"></iframe> -->
 	<div id="main">
-		<?php
-		$title=$Title->find(['sh'=>1]);
+		<?php 
+			$title=$Title->find(['sh'=>1]);
 		?>
-    	<a title="<?=$title['text'];?>" href="./index.php">
-		<div class="ti" style="background:url(&#39;./img/<?=$title['img'];?>&#39;); background-size:cover;"></div><!--標題--></a>
+    	<a title="<?=$title['text'];?>" href="index.php">
+			<div class="ti" style="background:url(&#39;./img/<?=$title['img'];?>&#39;); background-size:cover;"></div><!--標題--></a>
         	<div id="ms">
              	<div id="lf" style="float:left;">
             		<div id="menuput" class="dbor">
@@ -30,37 +29,25 @@
                     	                            <span class="t botli">主選單區</span>
                                                 </div>
                     <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-                    	<span class="t">進站總人數 :<?=$Total->find(1)['total'];?></span>
+                    	<span class="t">進站總人數 : <?=$Total->find(1)['total'];?></span>
                     </div>
         		</div>
-               <!--front-main-start-->
-			   <?php //include "./front/main.php"; ?>
-			   <?php
-			    // $do=(isset($_GET['do']))?$_GET['do']:'main';
-				// $do=$_GET['do']??'main';
-				//  include "./front/$do.php";
-			 $do=$_GET['do']??'main';
-			 $file = "./front/{$do}.php";
-			if (file_exists($file)) {
-			    include $file;
-			} else {
-    		include "./front/main.php";
-			}
-				// switch($do){
-				// 	case "$do" ;
-				// 	include "./front/$do.php";
-				// 	break;
-				// 	// case "news" ;
-				// 	// include "./front/news.php";
-				// 	// break;
-				// 	default:
-				// 	include "./front/main.php";
-				//    }
-				?>
-			   <!--front-main-end-->
-			   <div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
+
+				<?php 
+
+					$do=$_GET['do']??'main';
+					$file="./front/{$do}.php";
+					if(file_exists($file)){
+						include $file;
+					}else{
+						include "./front/main.php";	
+					}
+
+					?>
+
+                    <div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
                 	<!--右邊-->   
-                	<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo('?do=login')">管理登入</button>
+                	<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
                 	<div style="width:89%; height:480px;" class="dbor">
                     	<span class="t botli">校園映象區</span>
 						                        <script>
