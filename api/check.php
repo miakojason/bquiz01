b9422d39f309f19e906fc2a9491cf880c8721c69
miakojason <miakojason@gmail.com>
2024-02-26 09:08:10 +0800

    更改為源頭DB解決SQL隱碼攻擊，才不用每個表單都修改

--- api/check.php
+++ api/check.php
@@ -2,10 +2,11 @@
 include_once "./db.php";
 //' OR 1=1; <=SQL注入 隱碼攻擊
 //過濾表單資料
-$acc=htmlspecialchars($_POST['acc']);
-$pw=htmlspecialchars($_POST['pw']);
-if($Admin->count(['acc'=>$acc,'pw'=>$pw])>0){
-    $_SESSION['login']=$acc;
+// $acc=htmlspecialchars($_POST['acc']);
+// $pw=htmlspecialchars($_POST['pw']);
+//改為從源頭DB解決
+if($Admin->count(['acc'=>$_POST['acc'],'pw'=>$_POST['pw']])>0){
+    $_SESSION['login']=$_POST['acc'];
     to("../back.php?do=title");
 }else{
     to("../index.php?do=login&error=帳號密碼或錯誤");
